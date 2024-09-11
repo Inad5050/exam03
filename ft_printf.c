61b3dcbc5479beb6ec8429738b360e2450a1636e
Inad <danitorralba90@gmail.com>
2024-09-11 16:30:01 +0200

    miercoles2

--- ft_printf.c
+++ ft_printf.c
@@ -24,28 +24,28 @@ void	put_digit(long long int nbr, int base, int *len)
 	*len += write(1, &hexa[nbr % base], 1);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *str, ...)
 {
-	int			len;
-	va_list		ptr;
+	int			len = 0;
+	va_list		arg;
 
-	len = 0;
-	va_start(ptr, format);
-	while (*format)
+	va_start(arg, str);
+	while (*str)
 	{
-		if ((*format == '%') && *(format + 1))
+		if ((*str == '%') && *(str + 1))
 		{
-			format++;
-			if (*format == 's')
-				put_str(va_arg(ptr, char *), &len);
-			else if (*format == 'd')
-				put_digit((long long int)va_arg(ptr, int), 10, &len);
-			else if (*format == 'x')
-				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
+			str++;
+			if (*str == 's')
+				put_str(va_arg(arg, char *), &len);
+			else if (*str == 'd')
+				put_digit((long long int)va_arg(arg, int), 10, &len);
+			else if (*str == 'x')
+				put_digit((long long int)va_arg(arg, unsigned int), 16, &len);
 		}
 		else
-			len += write(1, format, 1);
-		format++;
+			len += write(1, str, 1);
+		str++;
 	}
-	return (va_end(ptr), len);
+	va_end(arg);
+	return (len);
 }
