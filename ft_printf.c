08e50cfdbdaf534f445bcc27c9dc217f20d0060d
Inad <danitorralba90@gmail.com>
2024-09-11 16:22:58 +0200

    miercoles1

--- ft_printf.c
+++ ft_printf.c
@@ -1,67 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dangonz3 <dangonz3@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/10 17:36:44 by dangonz3          #+#    #+#             */
-/*   Updated: 2024/09/10 20:17:19 by dangonz3         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdarg.h>
+#include <unistd.h>
 
-#include "stdlib.h"
-#include "unistd.h"
-#include "stdarg.h"
-
-void putstr(char *str, int *len)
+void	put_str(char *str, int *len)
 {
-    if (!str)
-        str = "(null)";
-    while (*str)
-        len += write(1, str, 1);
+	if (!str)
+		str = "(null)";
+	while (*str)
+		*len += write(1, str++, 1);
 }
 
-int ptf(const char *s, va_list args, int i, int len)
+void	put_digit(long long int nbr, int base, int *len)
 {
-    if (!s[i])
-        return (len);
-    if (s[i] == '%' && s[i + 1] == 's')
-        return (ptf(s, args, i + 2, len + putstr(va_arg(args, char *), &len)));
-    if (s[i] == '%' && s[i + 1] == 'd')
-        return (ptf(s, args, i + 2, len + putnum(va_arg(args, int), 10, &len)));
-    if (s[i] == '%' && s[i + 1] == 'x')
-        return (ptf(s, args, i + 2, len + putnum(va_arg(args, int), 16, &len)));
-    write(1, &s[i], 1);
-    return (ptf(s, args, i + 1, len + 1));
+	char	*hexa;
+
+	hexa = "0123456789abcdef";
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		*len += write(1, "-", 1);
+	}
+	if (nbr >= base)
+		put_digit((nbr / base), base, len);
+	*len += write(1, &hexa[nbr % base], 1);
 }
 
-int ft_printf(const char *s, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list args;
-    int     len;
-    
-    len = 0;
-    va_start(args, s);
-    len =+ ptf(s, args, 0, 0);
-    va_end(args);
-    return (len);
+	int			len;
+	va_list		ptr;
+
+	len = 0;
+	va_start(ptr, format);
+	while (*format)
+	{
+		if ((*format == '%') && *(format + 1))
+		{
+			format++;
+			if (*format == 's')
+				put_str(va_arg(ptr, char *), &len);
+			else if (*format == 'd')
+				put_digit((long long int)va_arg(ptr, int), 10, &len);
+			else if (*format == 'x')
+				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
+		}
+		else
+			len += write(1, format, 1);
+		format++;
+	}
+	return (va_end(ptr), len);
 }
-
-int putnum(int num, int base, int *len)
-{
-    char    *hexa = "0123456789abcdef";
-    
-    if (num < 0)
-    {
-        *len += write(1, "-", 1);
-        num *= -1;
-    }
-
-
-
-
-
-
-}
-
